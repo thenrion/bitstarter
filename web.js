407cdf97c5f04b42da945c35b140f7dd105402af
Thomas Henrion <thomas.henrion@gmail.com>
2013-07-22 22:14:10 +0000

    add )

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
     var output = '';
     fs.readFile('index.html', function (err, data) {
 	if (err) throw err;
-	response.send(new Buffer(data).toString();
+	response.send(new Buffer(data).toString());
     });
 });
 
