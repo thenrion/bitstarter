d847c4177fb83e0a9afa07796d189fd61a20469a
Thomas Henrion <thomas.henrion@gmail.com>
2013-07-22 21:36:47 +0000

    Read data from index.html file without Buffer

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var output = '';
+    fs.readFile('index.html', function (err, data) {
+	if (err) throw err;
+	output = data;
+    });
+    response.send(output);
 });
 
 var port = process.env.PORT || 5000;
